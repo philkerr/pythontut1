f4ed04a5d519de28591b2793f0cfe964e50a5960
Philippe K <git@adera.be>
2020-03-07 17:04:09 +0100

    changed return string to 07-mar-2020

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ app = Flask(__name__)
 @app.route('/')
 @app.route('/hello/')
 def hello_world():
-    return 'Hello World!\n'
+    return 'Hello World! at 07-Mar-2020 17:01\n'
 
 @app.route('/hello/<username>') # dynamic route
 def hello_user(username):
